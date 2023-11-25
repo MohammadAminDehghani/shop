57a5f01bc6c7b329ed876fe418a9632cba5b978b
php storm <man.dehghani1991@gmail.com>
2023-11-25 15:50:12 +0330

    payane 84

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata, ResolvingMetadata } from "next";
 import { toast } from "react-toastify";
 
 
@@ -19,12 +20,23 @@ const getProduct = async (id: string) => {
     return res.json();
 }
 
+export async function generateMetadata({params} : Props , parent : ResolvingMetadata) : Promise<Metadata> {
+    
+    let productData = await getProduct(params.id);
+    let product = productData.product;
+    
+    return {
+        title: product.title,
+        description: product.body.substr(0,120),
+    }
+}
+
 
 export default async function ShowProduct({params : {id}} : Props) {
 
     let productData = await getProduct(id);
     let product = productData.product;
     return(
-        <h2>product {product.title}</h2>
+        <h2>product { product.title }</h2>
     )
 }
